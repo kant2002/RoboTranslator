0dbfc4350d768827ac5f9ae2b993721c82963dd2
Andrii Kurdiumov <kant2002@gmail.com>
2025-03-01 12:59:47 +0500

    Testing CLI shape

--- Translator/Program.fs
+++ Translator/Program.fs
@@ -1,13 +1,42 @@
 ﻿open Karambolo.PO
+open Argu
+open System
 
-let parser = POParser();
-let result = parser.Parse("sample.po");
-if result.Success then
-    let catalog = result.Catalog
-    printfn "Parsed %d entries" catalog.Count
-else
-    let diagnostics = result.Diagnostics
-    for diagnostic in diagnostics do
-        printfn "Error: %s, Severity: %O" diagnostic.Code diagnostic.Severity
+type CliArguments =
+    | [<AltCommandLine("-v")>] Verbose
+    | [<AltCommandLine("-o")>] Output of output: string
+    | [<AltCommandLine("-s")>] Source of output: string
+    | [<AltCommandLine("-l")>] Language of language: string
 
-printfn "Hello from F#"
+    interface IArgParserTemplate with
+        member s.Usage =
+            match s with
+            | Verbose -> "Verbose output"
+            | Source _ -> ".po/.pot file to translate"
+            | Output _ -> ".po file where to save translation"
+            | Language _ -> "Language to which translate file"
+
+[<EntryPoint>]
+let main argv =
+    let errorHandler = ProcessExiter(colorizer = function ErrorCode.HelpText -> None | _ -> Some ConsoleColor.Red)
+    let parser = ArgumentParser.Create<CliArguments>(programName = "robotranslator", errorHandler = errorHandler)
+    let results = parser.ParseCommandLine (inputs=argv, ignoreUnrecognized = true)
+
+    let sourceFileName = results.GetResult(CliArguments.Source)
+    let outputFileName = results.GetResult(CliArguments.Output)
+
+
+    let parser = POParser();
+    do
+        use stream = new System.IO.FileStream(sourceFileName, System.IO.FileMode.Open)
+        let result = parser.Parse(stream);
+        if result.Success then
+            let catalog = result.Catalog
+            printfn "Parsed %d entries" catalog.Count
+            let targetLanguage = results.GetResult(CliArguments.Language)
+            printfn "Source language %s. Translating to %s" catalog.Language targetLanguage
+        else
+            let diagnostics = result.Diagnostics
+            for diagnostic in diagnostics do
+                printfn "Error: %s, Severity: %O. %A" diagnostic.Code diagnostic.Severity diagnostic.Args
+    0
